50a7d0642dde11b2047c027c13948a6294ec17ff
aarai <zclinux@gmail.com>
2014-09-10 07:36:31 -0400

    add local dev settings

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'ideas',
-			'username'  => 'root',
-			'password'  => 'football',
+			'host'      => '',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
